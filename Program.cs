3b626008bc2740820dacf2cd67b7cd4d025f51c1
jiteshwari sahu <sahujiteshwari19@gmail.com>
2018-03-23 19:43:52 +0530

    Json

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Json
+{
+    public class A
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonData = "{  \"FirstName\":\"Jitu\",\"LastName\":\"Sahu\" }";
+            dynamic data = JObject.Parse(jsonData);
+
+            Console.WriteLine(string.Concat("Hi ", data.FirstName, " " + data.LastName));
+            Console.ReadKey();
+        }
+    }
+}
